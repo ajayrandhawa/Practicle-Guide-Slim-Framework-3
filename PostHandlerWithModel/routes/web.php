bd599213a60663bd8e82f37fd42eebd2b90c5c60
Ajay Randhawa <ajayrandhawartg@gmail.com>
2019-03-16 23:07:41 +0530

    Add files via upload

--- PostHandlerWithModel/routes/web.php
+++ PostHandlerWithModel/routes/web.php
@@ -8,3 +8,6 @@ use \App\Controllers\UserController;
 $app->get('/users', UserController::class.':index');
 $app->get('/posts', TopicController::class.':index');
 $app->get('/posts/{id}', TopicController::class.':show')->setName('posts.show');
+
+$app->get('/topics', TopicController::class.':addTopic')->setName('topic.add');
+$app->get('/topics/{id}', TopicController::class.':showTopic')->setName('topic.show');
